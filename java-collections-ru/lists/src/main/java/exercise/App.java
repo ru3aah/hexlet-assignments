b6724135a466e6576ed207c58a2e4aea90fcd8fb
@hexlet/cli <support@hexlet.io>
2023-03-07 10:20:56 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,40 +1,32 @@
 package exercise;
-import java.util.LinkedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
 // BEGIN
-public class App {
+class App {
 
-    public static Boolean scrabble(String symbSet, String inword) {
-
-        if (inword.length() > symbSet.length()) {
-            return false;
-        }
+    public static boolean scrabble(String symbSet, String inword) {
 
         String word = inword.toLowerCase();
-        List<Character> symbList = new LinkedList<>();
-        List<Character> wordList = new LinkedList<>();
+        String[] letters = symbSet.split("");
+        List symbList = new ArrayList(Arrays.asList(letters));
 
-        //convert symSet String to symbList List of chars
         for (int i = 0; i < symbSet.length(); i++) {
             symbList.add(symbSet.charAt(i));
         }
         //convert word  String to wordList list of chars
-        for (int i = 0; i < word.length(); i++) {
-            wordList.add(word.charAt(i));
-        }
-        //check if symbList has got enough suitable symbols
-        List tmp = wordList;
-        for (int i = 0; i < tmp.size(); i++){
-            if (symbList.contains(tmp.get(i))) {
-                symbList.remove(tmp.get(i));
-                wordList.remove(tmp.get(i));
+        for (int i = 0; i < inword.length(); i++) {
+            String tmp = word.substring(i,i+1);
+
+            if (!symbList.contains(tmp)) {
+                return false;
             }
+            symbList.remove(tmp);
         }
-
-
-        return wordList.isEmpty();
+        return true;
     }
 }
 

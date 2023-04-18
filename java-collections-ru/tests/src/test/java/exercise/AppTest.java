70a462ccd5ee400946ade970c8c00751f86aa74f
@hexlet/cli <support@hexlet.io>
2023-04-18 16:05:27 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -2,7 +2,7 @@ package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-//import java.util.Arrays;
+import java.util.Arrays;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
@@ -10,7 +10,6 @@ class AppTest {
 
     @Test
     void testTake() {
-
         // BEGIN
         List<Integer> listFull = new ArrayList<>(1, 2, 3, 4, 5);
         List<Integer> listEmpty = new ArrayList<>();

30a93e880660404b5d5daf35ca9961f38f5084e2
@hexlet/cli <support@hexlet.io>
2023-04-18 16:13:47 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -2,7 +2,7 @@ package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 

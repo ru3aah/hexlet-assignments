c727c4222af866d0d86a1f06d49f81977f83653f
@hexlet/cli <support@hexlet.io>
2023-04-18 15:33:13 +0300

    submit java-collections-ru/tests

--- java-collections-ru/tests/src/test/java/exercise/AppTest.java
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -2,7 +2,7 @@ package exercise;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import java.util.List;
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 
@@ -12,14 +12,14 @@ class AppTest {
     void testTake() {
 
         // BEGIN
-        List<Integer> listFull = new ArrayList<>(1,2,3,4,5);
+        List<Integer> listFull = new ArrayList<>(1, 2, 3, 4, 5);
         List<Integer> listEmpty = new ArrayList<>();
         int elemQtyNorm = 3;
         int elemQtyExceed = 7;
-        assertThat(take(listFull, elemQtyNorm)).isEqualTo(1,2,3);
-        assertThat(take(listFull, elemQtyExceed)).isEqalTo(1,2,3,4,5);
-        assertThat(take(listFull,0)).isEmpty;
-        assertThat(take(listEmpty, elemQtyNorm )).isEmpty;
+        assertThat(take(listFull, elemQtyNorm)).isEqualTo(1, 2, 3);
+        assertThat(take(listFull, elemQtyExceed)).isEqalTo(1, 2, 3, 4, 5);
+        assertThat(take(listFull, 0)).isEmpty;
+        assertThat(take(listEmpty, elemQtyNorm)).isEmpty;
         // END
     }
 }

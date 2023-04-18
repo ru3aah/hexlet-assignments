ba2684e9c198b3fb8a93b20f4e86c1b877170015
@hexlet/cli <support@hexlet.io>
2023-04-18 15:20:42 +0300

    submit java-collections-ru/tests

---
+++ java-collections-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @Test
+    void testTake() {
+        // BEGIN
+       int List listFull = new ArrayList<>(1,2,3,4,5);
+       int List listEmpty = new ArrayList<>();
+       int elemQtyNorm = 3;
+       int elemQtyExceed = 7;
+       assertThat(take(listFull, elemQtyNorm)).isEqualTo(1,2,3);
+       assertThat(take(listFull, elemQtyExceed)).isEqalTo(1,2,3,4,5);
+       assertThat(take(listFull,0)).isEmpty;
+       assertThat(take(listEmpty, elemQtyNorm )).isEmpty;
+        // END
+    }
+}

1e9cfc0318523ca2a4dcf7e95e679c799a580a34
@hexlet/cli <support@hexlet.io>
2023-07-26 22:38:02 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -1,7 +1,11 @@
 package exercise;
 
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 //import java.util.Map;
 
 

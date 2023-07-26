8ae0e1ea8996a1c0350ffc73383569ab48edf313
@hexlet/cli <support@hexlet.io>
2023-07-26 22:25:06 +0300

    submit java-oop-ru/reflections

---
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -0,0 +1,52 @@
+package exercise;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+
+// BEGIN
+class Validator {
+    public static List<String> validate(Address address) {
+        List<String> nullFields = new ArrayList<>();
+        try {
+            for(Field f : address.getClass().getDeclaredFields()) {
+                try {
+                    if ((f.getAnnotation(NotNull.class).getClass()).equals(NotNull.class)) {
+                        if (Objects.isNull(f.get(new Object()))) {
+                            nullFields.add(f.getName());
+                        }
+                    }
+                } catch (IllegalAccessException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Smth went wrong");
+        }
+        return nullFields;
+    }
+    public static Map<String,List<String>> advanceValidate(Address address) throws IllegalAccessException {
+        Map<String, List<String>> result = new HashMap<>();
+        List<String> errorList = new ArrayList<>();
+        for(Field f : address.getClass().getDeclaredFields()) {
+         errorList.clear();
+                if ((f.getAnnotation(NotNull.class).getClass()).equals(NotNull.class)) {
+                    if (Objects.isNull(f.get(new Object()))) {
+                        errorList.add("is null");
+                    }
+                }
+                if (!f.isAccessible()) {
+                    f.setAccessible(true);
+                }
+                String value = (String) f.get(new Object());
+                if(value.length() < f.getAnnotation(MinLength.class).minLength()) {
+                    errorList.add("length is less than allowed minimum");
+                }
+                if(!errorList.isEmpty()) {
+                    result.put(f.getName(), errorList);
+                }
+        }
+        return result;
+    }
+}
+// END

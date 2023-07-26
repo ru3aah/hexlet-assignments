7019d59f8caea4e482eb6fa205765b0f394d03b7
@hexlet/cli <support@hexlet.io>
2023-07-26 22:35:33 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -1,11 +1,8 @@
 package exercise;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
+//import java.util.Map;
 
 
 // BEGIN
@@ -39,18 +36,18 @@ class Validator {
             if ((f.getAnnotation(NotNull.class).getClass())
                     .equals(NotNull.class)) {
                 if (Objects.isNull(f.get(new Object()))) {
-                        errorList.add("is null");
+                    errorList.add("is null");
                 }
             }
             if (!f.isAccessible()) {
-                    f.setAccessible(true);
+                f.setAccessible(true);
             }
             String value = (String) f.get(new Object());
-            if(value.length() < f.getAnnotation(MinLength.class).minLength()) {
+            if (value.length() < f.getAnnotation(MinLength.class).minLength()) {
                 errorList.add("length is less than allowed minimum");
             }
-            if(!errorList.isEmpty()) {
-                    result.put(f.getName(), errorList);
+            if (!errorList.isEmpty()) {
+                result.put(f.getName(), errorList);
             }
         }
         return result;
